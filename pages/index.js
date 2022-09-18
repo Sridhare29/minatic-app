2ab2fe00e4ace19819feedb4d1ca75f218a3a0d5
pss-coder <pawandeepse@me.com>
2022-09-18 11:23:32 +0200

    FIX Authentication

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 //import minaticLogo from "../styles/minatic.png"
 
+import { useSession, signIn, signOut, getSession } from 'next-auth/react';
+
 // server-side rendering
 // to pass session with other props data
 export async function getServerSideProps(context) {
@@ -43,24 +45,10 @@ export default function Index({session}) {
           <code className={styles.code}>Your Minutes Secretary</code>
         </p>
 
-        <div className={styles.grid}>
-
-          <Link href="/minatic/view">
-            <a className={styles.card}>
-              <h2 className='text-3xl font-bold underline'>View My Minutes</h2>
-              <p>View all minutes</p>
-            </a>
-          </Link>
-          
-          <Link href="/minatic/start">
-            <a className={styles.card}>
-              <h2 className='text-3xl font-bold underline'>Start Meeting</h2>
-              <p>Start meeting</p>
-            </a>
-          </Link>
-        </div>
+        <button className='bg-blue-300 hover:bg-slate-500 hover:text-white py-2 px-4 rounded-full' onClick={() => signIn()}>Sign In</button>
 
         <p>Still under development...</p>
+
       </main>
 
       <footer className={styles.footer}>
